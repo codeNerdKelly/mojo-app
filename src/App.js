da97150b6cdd4eaa114ec5a6f1ff1acc889305e6
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-08-16 11:12:44 -0400

    project structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather WOW</h1>
+      <footer>
+        <a href ="codenerdkelly.me" target= "_blank">
+          made by codeNerdKelly
+        </a>
+        <br></br>
+        <a href="/">repo</a>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
