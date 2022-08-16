51dfd359fde739d8a058302ddd5bca68ee89f1de
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-08-16 15:36:38 -0400

    adding rel=noreferrer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div className="App">
       <h1>Weather WOW</h1>
       <footer>
-        <a href ="codenerdkelly.me" target= "_blank">
+        <a href ="codenerdkelly.me" target= "_blank" rel="noreferrer">
           made by codeNerdKelly
         </a>
         <br></br>
-        <a href="/">repo</a>
+        <a href="https://github.com/codeNerdKelly/mojo-app" target= "_blank" rel="noreferrer">repo</a>
       </footer>
     </div>
   );
