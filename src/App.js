aa532d49f2ef79908b9cf80e4814349f92b1ab51
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-08-16 15:51:54 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className = "container">
       <h1>Weather WOW</h1>
       <footer>
         <a href ="codenerdkelly.me" target= "_blank" rel="noreferrer">
@@ -12,6 +13,7 @@ function App() {
         <br></br>
         <a href="https://github.com/codeNerdKelly/mojo-app" target= "_blank" rel="noreferrer">repo</a>
       </footer>
+     </div>
     </div>
   );
 }
